d4fd4d506248652622c9afb1f4bce0ffefca7726
sushil <sushilwaghe2169@gmail.com>
2023-12-09 15:25:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Footer from './customer/components/Footer/Footer';
+import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import HomePage from './customer/pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="">
+    <Navigation />
+    <div>
+    {/* <HomePage /> */}
+    {/* <Product /> */}
+    <ProductDetails />
+    </div>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
